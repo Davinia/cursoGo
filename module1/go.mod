47e3606aa0d7e76358a3556b388085ae02512196
Davinia Tomé González (edavtom) <davinia.tome.gonzalez@ericsson.com>
2024-11-04 18:56:51 +0100

    Fix import path

--- module1/go.mod
+++ module1/go.mod
@@ -1,3 +1,5 @@
 module github.com/Davinia/cursoGo/module1
 
 go 1.23.2
+
+require github.com/Davinia/cursoGo/module1/package1 v0.0.0-20241104171717-9dfef7c19858
--- module1/main.go
+++ module1/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/Davinia/cursoGo/module1/package1"
+	_ "github.com/Davinia/cursoGo/module1/package1"
 )
 
 func init() {
