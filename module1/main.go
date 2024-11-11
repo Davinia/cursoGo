d8a2dc4c42eb56268ece55d70f9a52b75cd642ba
Davinia Tomé González (edavtom) <davinia.tome.gonzalez@ericsson.com>
2024-11-11 16:19:21 +0100

    Exercise 1

--- module1/main.go
+++ module1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	_ "github.com/Davinia/cursoGo/module1/package1"
 )
 
@@ -12,4 +13,11 @@ func init() {
 func main() {
 	fmt.Println("You are in main entry point from package main")
 	fmt.Println("Welcome")
+	a := 10
+	b := 20
+	c := 30
+	d := 40
+	e := 50
+	f := 60
+	fmt.Println(a + b + c + d + e + f)
 }
